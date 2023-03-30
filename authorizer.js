fb7b250bcb15cb29cba750e89f231d985313afbb
Gabriel Falcones <gafalcon@espol.edu.ec>
2023-03-30 12:28:17 -0500

    cognito authorizer

--- authorizer.js
+++ authorizer.js
@@ -1,17 +1,22 @@
-module.exports.handler = function (event, context, callback) {
+const { CognitoJwtVerifier } = require("aws-jwt-verify");
+
+const COGNITO_USERPOOL_ID = process.env.COGNITO_USERPOOL_ID;
+const COGNITO_WEB_CLIENT_ID = process.env.COGNITO_WEB_CLIENT_ID;
+
+const jwtVerifier = CognitoJwtVerifier.create({
+  userPoolId: COGNITO_USERPOOL_ID,
+  tokenUse: "id",
+  clientId: COGNITO_WEB_CLIENT_ID,
+});
+
+module.exports.handler = async function (event, context, callback) {
   const token = event.authorizationToken;
-  switch (token) {
-    case "allow":
-      callback(null, generatePolicy("user", "Allow", event.methodArn));
-      break;
-    case "deny":
-      callback(null, generatePolicy("user", "Deny", event.methodArn));
-      break;
-    case "unauthorized":
-      callback("Unauthorized"); // Return a 401 Unauthorized response
-      break;
-    default:
-      callback("Error: Invalid token"); // Return a 500 Invalid token response
+  try {
+    const payload = await jwtVerifier.verify(token);
+    console.log(JSON.stringify(payload));
+    callback(null, generatePolicy("user", "Allow", event.methodArn));
+  } catch (err) {
+    callback("Error: Invalid token"); // Return a 500 Invalid token response
   }
 };
 
